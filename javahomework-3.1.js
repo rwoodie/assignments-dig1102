230e907235060be70c7e436495f66b377619057f
OpenShift guest <woodie41@hotmail.com>
2013-01-29 00:21:55 -0500

    Cleaned up the code to make it easier to read

--- javahomework-3.1.js
+++ javahomework-3.1.js
@@ -1,16 +1,24 @@
 <script type="text/javascript">
-    currentValue = 100;
-    targetValue = 1500;
+    var currentValue = 100;
+    var targetValue = 1500;
 // Should use var to name variables currentValue and targetValue 
-    function count() {
-        if (currentValue > targetValue) {
+    function count()
+    {
+        if (currentValue > targetValue) 
+        {
         currentValue -= 1
-        } else if (currentValue < targetValue) {
+        } 
+        
+        else if (currentValue < targetValue) 
+        {
             currentValue += 1
         } 
+        
         document.getElementById('myDiv').innerHTML = 'Total wordcount:'+ currentValue.toString();
         changeTime = 20;
-        if (Math.abs(currentValue - targetValue) < 980) {
+        
+        if (Math.abs(currentValue - targetValue) < 980) 
+        {
             changeTime = 1000 - Math.abs(currentValue - targetValue);
         }
         setTimeout(count,changeTime/2);
