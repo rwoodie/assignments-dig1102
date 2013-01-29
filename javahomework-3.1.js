900823ad5f4c47e7c0acff257974e8a039783ba9
OpenShift guest <woodie41@hotmail.com>
2013-01-28 23:47:03 -0500

    Commented on ways to cleanup the code

---
+++ javahomework-3.1.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+    currentValue = 100;
+    targetValue = 1500;
+// Should use var to name variables currentValue and targetValue 
+    function count() {
+        if (currentValue > targetValue) {
+        currentValue -= 1
+        } else if (currentValue < targetValue) {
+            currentValue += 1
+        } 
+        document.getElementById('myDiv').innerHTML = 'Total wordcount:'+ currentValue.toString();
+        changeTime = 20;
+        if (Math.abs(currentValue - targetValue) < 980) {
+            changeTime = 1000 - Math.abs(currentValue - targetValue);
+        }
+        setTimeout(count,changeTime/2);
+    }
+count()
+// Using the almond style for brackets could make the code cleaner
+</script> 
