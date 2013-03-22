7ae4d2ec8c1708a87a8972a996d0d0a99e17b0ab
OpenShift guest <woodie41@hotmail.com>
2013-03-21 23:06:50 -0400

    entered all addition assert tests successfully

--- Homework-9.1.js
+++ Homework-9.1.js
@@ -43,34 +43,4 @@ var addition = function(a, b)
     assert.equal(addition(-1, -1), -2);
     assert.equal(addition(-1, +0), -1);
 }
-    
-/**
- * math | one | two | value
- * ===+=====+=====+======
- * -    | +1  | +1  | +0
- * -    | +1  | -1  | +2
- * -    | +1  | +0  | +1
- * -    | +0  | +1  | -1
- * -    | +0  | -1  | +1
- * -    | +0  | +0  | +0
- * -    | -1  | +1  | -2
- * -    | -1  | -1  | +0
- * -    | -1  | +0  | -1
- */
- 
- //This function is for subtraction
- var subtraction = function(a, b)
- {
-     return a - b;
- }
- 
-    assert.equal(subtraction(+1, +1), +0);
-    assert.equal(subtraction(+1, -1), +2);
-    assert.equal(subtraction(+1, +0), +1);
-    assert.equal(subtraction(+0, +1), -1);
-    assert.equal(subtraction(+0, -1), +1);
-    assert.equal(subtraction(+0, +0), +0);
-    assert.equal(subtraction(-1, +1), -2);
-    assert.equal(subtraction(-1, -1), +0);
-    assert.equal(subtraction(-1, +0), -1);
-    
+
