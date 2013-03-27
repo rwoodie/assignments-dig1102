76b7277f6445ea8abad7b160865887486f73921f
OpenShift guest <woodie41@hotmail.com>
2013-03-26 23:12:36 -0400

    got a failing test as there is no function defined for the addition

---
+++ Homework-10.1.js
@@ -0,0 +1,29 @@
+var assert = require("assert")
+
+/**
+ * four functions with two parameters:
+ * - addition
+ * - subtraction
+ * - multiplication
+ * - division
+ * 
+ * Must accept positive and negative Numbers (int or 
+ * float) AND zero. Must not make an error!
+ */
+
+
+/**
+ * math | one | two | value
+ * ===+=====+=====+======
+ * +    | +1  | +1  | +2
+ * +    | +1  | -1  | +0
+ * +    | +1  | +0  | +1
+ * +    | +0  | +1  | +1
+ * +    | +0  | -1  | -1
+ * +    | +0  | +0  | +0
+ * +    | -1  | +1  | +0
+ * +    | -1  | -1  | -2
+ * +    | -1  | +0  | -1
+ */
+ 
+assert.equal(addition(+1, +1), +2);
