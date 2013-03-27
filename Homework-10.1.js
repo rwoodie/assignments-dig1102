21de6be97e77b23303d6353cd734ca9c75877528
OpenShift guest <woodie41@hotmail.com>
2013-03-26 23:57:17 -0400

    got a failing test for subtraction assert test

--- Homework-10.1.js
+++ Homework-10.1.js
@@ -40,3 +40,19 @@ var addition = function(a, b){
     assert.equal(addition(-1, -1), -2);
     assert.equal(addition(-1, +0), -1);
     
+/**
+ * math | one | two | value
+ * ===+=====+=====+======
+ * -    | +1  | +1  | +0
+ * -    | +1  | -1  | +2
+ * -    | +1  | +0  | +1
+ * -    | +0  | +1  | -1
+ * -    | +0  | -1  | +1
+ * -    | +0  | +0  | +0
+ * -    | -1  | +1  | -2
+ * -    | -1  | -1  | +0
+ * -    | -1  | +0  | -1
+ */
+ 
+ assert.equal(subtraction(+1, +1), +0);
+ 
