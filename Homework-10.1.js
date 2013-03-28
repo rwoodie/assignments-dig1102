714f3e78e648ba13c28068c1b2fa8eea04996752
OpenShift guest <woodie41@hotmail.com>
2013-03-28 00:33:07 -0400

    got a failing test for first assert test

--- Homework-10.1.js
+++ Homework-10.1.js
@@ -1,4 +1,4 @@
-gvar assert = require("assert")
+var assert = require("assert")
 
 /**
  * four functions with two parameters:
@@ -26,45 +26,4 @@ gvar assert = require("assert")
  * +    | -1  | +0  | -1
  */
  
-var addition = function(a, b){
-    return a + b;
-}
-
-    assert.equal(addition(+1, +1), +2);
-    assert.equal(addition(+1, -1), +0);
-    assert.equal(addition(+1, +0), +1);
-    assert.equal(addition(+0, +1), +1);
-    assert.equal(addition(+0, -1), -1);
-    assert.equal(addition(+0, +0), +0);
-    assert.equal(addition(-1, +1), +0);
-    assert.equal(addition(-1, -1), -2);
-    assert.equal(addition(-1, +0), -1);
-    
-/**
- * math | one | two | value
- * ===+=====+=====+======
- * -    | +1  | +1  | +0
- * -    | +1  | -1  | +2
- * -    | +1  | +0  | +1
- * -    | +0  | +1  | -1
- * -    | +0  | -1  | +1
- * -    | +0  | +0  | +0
- * -    | -1  | +1  | -2
- * -    | -1  | -1  | +0
- * -    | -1  | +0  | -1
- */
-
-var subtraction = function(a, b){
-    return a - b;
-}
- 
-    assert.equal(subtraction(+1, +1), +0);
-    assert.equal(subtraction(+1, -1), +2);
-    assert.equal(subtraction(+1, +0), +1);
-    assert.equal(subtraction(+0, +1), -1);
-    assert.equal(subtraction(+0, -1), +1);
-    assert.equal(subtraction(+0, +0), +0);
-    assert.equal(subtraction(-1, +1), -2);
-    assert.equal(subtraction(-1, -1), +0);
-    assert.equal(subtraction(-1, +0), -1);
- 
+assert.equal(addition(+1, +1), +2);
