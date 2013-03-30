60a9c8b858ca2ed534694281eb25873b201af9b1
OpenShift guest <woodie41@hotmail.com>
2013-03-30 01:08:25 -0400

    Got a failing assert test

--- Homework-10.1.js
+++ Homework-10.1.js
@@ -15,36 +15,6 @@ var assert = require("assert")
 /**
  * math | one | two | value
  * ===+=====+=====+======
- * +    | +1  | +1  | +2
- * +    | +1  | -1  | +0
- * +    | +1  | +0  | +1
- * +    | +0  | +1  | +1
- * +    | +0  | -1  | -1
- * +    | +0  | +0  | +0
- * +    | -1  | +1  | +0
- * +    | -1  | -1  | -2
- * +    | -1  | +0  | -1
- */
-
-//Testing addition
-var addition = function(a, b){
-
-    return a + b;
-}
-
-    assert.equal(addition(+1, +1), +2);
-    assert.equal(addition(+1, -1), +0);
-    assert.equal(addition(+1, +0), +1);
-    assert.equal(addition(+0, +1), +1);
-    assert.equal(addition(+0, -1), -1);
-    assert.equal(addition(+0, +0), +0);
-    assert.equal(addition(-1, +1), +0);
-    assert.equal(addition(-1, -1), -2);
-    assert.equal(addition(-1, +0), -1);
-    
-/**
- * math | one | two | value
- * ===+=====+=====+======
  * /    | +1  | +1  | +1
  * /    | +1  | -1  | -1
  * /    | +1  | +0  | +0
@@ -56,25 +26,4 @@ var addition = function(a, b){
  * /    | -1  | +0  | +0
  */
 
-//A test for division
-var divide = function(a, b){
-    
-       if (a == +0 || b == +0)
-       {
-       console.log('not divisible by zero');
-       }  
-     
-       return (a / b);
-           
-}
-
-    assert.equal(divide(+1, +1), +1);
-    assert.equal(divide(+1, -1), -1);
-    assert.equal(divide(+1, +0), +0);
-    assert.equal(divide(+0, +1), +0);
-    assert.equal(divide(+0, -1), +0);
-    assert.equal(divide(+0, +0), +0);
-    assert.equal(divide(-1, +1), -1);
-    assert.equal(divide(-1, -1), +1);
-    assert.equal(divide(-1, +0), +0);
-    
+assert.equal(division(+1, +1), +1);
