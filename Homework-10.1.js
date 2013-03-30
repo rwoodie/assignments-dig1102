1fdd317efe0225c19062bd58f535c50ccf27fa27
OpenShift guest <woodie41@hotmail.com>
2013-03-30 01:41:19 -0400

    included guard clause to correct the divisible by zero error

--- Homework-10.1.js
+++ Homework-10.1.js
@@ -31,6 +31,7 @@ var division = function(a, b){
     if(!a || !b)
     {
         return 0;
+        echo 'Not divisible by zero'
     }
     return a / b;
 
@@ -39,4 +40,11 @@ var division = function(a, b){
     assert.equal(division(+1, +1), +1);
     assert.equal(division(+1, -1), -1);
     assert.equal(division(+1, +0), +0);
+    assert.equal(division(+0, +1), +0);
+    assert.equal(division(+0, -1), +0);
+    assert.equal(division(+0, +0), +0);
+    assert.equal(division(-1, +1), -1);
+    assert.equal(division(-1, -1), +1);
+    assert.equal(division(-1, +0), +0);
+    
     
