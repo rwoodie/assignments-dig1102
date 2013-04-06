777f8cc46ee2a531ee18b55c1cdd61f1c18dcc8f
OpenShift guest <woodie41@hotmail.com>
2013-04-05 22:19:01 -0400

    Completed remaining tests for addition. Got a passing test for all

--- Homework-11.1.js
+++ Homework-11.1.js
@@ -30,8 +30,15 @@ var addition = function(a, b){
     return a + b;
 }
  
- assert.equal(addition(+1, +1), +2);
- assert.equal(addition(+1, -1), +0);
+    assert.equal(addition(+1, +1), +2);
+    assert.equal(addition(+1, -1), +0);
+    assert.equal(addition(+1, +0), +1);
+    assert.equal(addition(+0, +1), +1);
+    assert.equal(addition(+0, -1), -1);
+    assert.equal(addition(+0, +0), +0);
+    assert.equal(addition(-1, +1), +0);
+    assert.equal(addition(-1, -1), -2);
+    assert.equal(addition(-1, +0), -1);
  
 
  
