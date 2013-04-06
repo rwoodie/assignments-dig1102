37c7e01ad45d25cb1dd66a63caa658f43e2a8a1a
OpenShift guest <woodie41@hotmail.com>
2013-04-05 22:41:30 -0400

    Completed remaining subtraction tests. All tests passed

--- Homework-11.1.js
+++ Homework-11.1.js
@@ -63,6 +63,12 @@ var subtraction = function(a, b){
     assert.equal(subtraction(+1, +1), +0);
     assert.equal(subtraction(+1, -1), +2);
     assert.equal(subtraction(+1, +0), +1);
+    assert.equal(subtraction(+0, +1), -1);
+    assert.equal(subtraction(+0, -1), +1);
+    assert.equal(subtraction(+0, +0), +0);
+    assert.equal(subtraction(-1, +1), -2);
+    assert.equal(subtraction(-1, -1), +0);
+    assert.equal(subtraction(-1, +0), -1);
 
  
 
