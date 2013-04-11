ffb007220bf6b8ea2ed195c55c0bbbf9bbc6fc57
OpenShift guest <woodie41@hotmail.com>
2013-04-11 01:17:28 -0400

    got a failing test for first subtraction assert

--- Homework-12.1.js
+++ Homework-12.1.js
@@ -25,11 +25,26 @@ var assert = require("assert")
  * +    | -1  | +0  | -1
  */
  
+ /**
+ * op | one | two | value
+ * ===+=====+=====+======
+ * -    | +1  | +1  | +0
+ * -    | +1  | -1  | +2
+ * -    | +1  | +0  | +1
+ * -    | +0  | +1  | -1
+ * -    | +0  | -1  | +1
+ * -    | +0  | +0  | +0
+ * -    | -1  | +1  | -2
+ * -    | -1  | -1  | +0
+ * -    | -1  | +0  | -1
+ */
+ 
 var addition = function(a, b)
 {
     return a + b;
 }
 
+{
     assert.equal(addition(+1, +1), +2);
     assert.equal(addition(+1, -1), +0);
     assert.equal(addition(+1, +0), +1);
@@ -39,3 +54,8 @@ var addition = function(a, b)
     assert.equal(addition(-1, +1), +0);
     assert.equal(addition(-1, -1), -2);
     assert.equal(addition(-1, +0), -1);
+}
+
+    assert.equal(subtraction(+1, +1), +0);
+    
+ 
