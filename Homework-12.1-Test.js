c15f3ea142daff90b244cde39a480167e27182f9
OpenShift guest <woodie41@hotmail.com>
2013-04-16 13:36:54 -0400

    Failing test for doors in hallway

--- Homework-12.1-Test.js
+++ Homework-12.1-Test.js
@@ -3,3 +3,22 @@ var assert = require("assert"),
     
 // test: do we have a Hallway?
 assert(hundredDoors.hallway);
+
+// test: do we have Doors in our Hallway?
+assert(hundredDoors.hallway.doors);
+
+// test: do we have 100 Doors?
+
+// test: are all the Doors "closed"?
+
+// do: PASS 1 -- take a walk down the hallway, change the state of the doors
+
+// test: are all the Doors "open"?
+
+// do: PASS 2 -- take a walk down the hallway, change the state of the doors
+
+// test: is every EVEN Door "closed"? is every ODD Door "open"?
+
+// do: PASS 3 -- take a walk down the hallway, change the state of the doors
+
+// test: does the Hallway match our expected state?
